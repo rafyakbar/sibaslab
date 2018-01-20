4bd30d32b972ca3cfebdd641e761c1f8cf321dc1
Rafy Aulia Akbar <rafy683@gmail.com>
2018-01-20 18:31:17 +0700

    update mahasiswa tabel

--- database/migrations/2018_01_20_063552_mahasiswa_table.php
+++ database/migrations/2018_01_20_063552_mahasiswa_table.php
@@ -24,6 +24,7 @@ class MahasiswaTable extends Migration
                 ->onUpdate('CASCADE')
                 ->onDelete('SET NULL');
             $table->string('nama');
+            $table->text('token');
             $table->timestamps();
         });
     }
