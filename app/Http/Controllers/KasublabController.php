af4693f5413044a6da5eeab8af15c8c9ee309575
BagasMuharom <bagashidayat45@gmail.com>
2018-01-23 18:56:31 +0700

    Update beberapa controller

--- app/Http/Controllers/KasublabController.php
+++ app/Http/Controllers/KasublabController.php
@@ -3,18 +3,71 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use App\Prodi;
+use App\User;
 
 class KasublabController extends Controller
 {
-    
-    public function tambah(Request $request)
+
+    public function __construct()
     {
-        
+        $this->middleware('kalab')->only([
+            'tambah', 'hapus'
+        ]);
     }
     
+    /**
+     * Menambah kasublab
+     *
+     * @param Request $request
+     * @return Illuminate\Http\JsonResponse
+     */
+    public function tambah(Request $request)
+    {
+        $this->validate($request, [
+            'nama' => 'required|string',
+            'nip' => 'required|numeric|unique:users',
+            'prodi' => [
+                'required',
+                Rule::in(Prodi::all()->pluck('id')->toArray())
+            ]
+        ]);
+
+        User::create([
+            'id' => $request->nip,
+            'nama' => $request->nama,
+            'prodi_id' => $request->prodi
+        ]);
+
+        return response()->json([
+            'success' => 'Berhasil menambahkan kasublab !'
+        ]);
+    }
+    
+    /**
+     * Menghapus kasublab
+     *
+     * @param Request $request
+     * @return Illuminate\Http\JsonResponse
+     */
     public function hapus(Request $request)
     {
+        try {
+            $user = User::findOrFail($request->nip);
 
+            $user->delete();
+
+            return response()->json([
+                'success' => 'Berhasil menghapus kasublab !'
+            ]);
+        }
+        catch (ModelNotFoundException $e) {
+            return response()->json([
+                'error' => 'Gagal menghapus kasublab. Tidak dapat menemukan kasublab tersebut !'
+            ]);
+        }
     }
 
     public function ubahProfil(Request $request)
@@ -24,7 +77,7 @@ class KasublabController extends Controller
 
     public function ubahKataSandi(Request $request)
     {
-        
+
     }
 
 }
