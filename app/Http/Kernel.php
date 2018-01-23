5e155902ca9e1019f8d7bfdbcd82892fdbf7d91c
BagasMuharom <bagashidayat45@gmail.com>
2018-01-23 11:43:28 +0700

    Menghapus middleware mobile checking

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,7 +57,6 @@ class Kernel extends HttpKernel
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'mobile' => \App\Http\Middleware\MobileCheck::class
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class
     ];
 }
