e12526494891e655f536b783518ef5f2a836da87
BagasMuharom <bagashidayat45@gmail.com>
2018-01-23 17:34:37 +0700

    Menambah beberapa controller

---
+++ app/Http/Controllers/KasublabController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class KasublabController extends Controller
+{
+    
+    public function tambah(Request $request)
+    {
+        
+    }
+    
+    public function hapus(Request $request)
+    {
+
+    }
+
+    public function ubahProfil(Request $request)
+    {
+
+    }
+
+    public function ubahKataSandi(Request $request)
+    {
+        
+    }
+
+}
---
+++ app/Http/Controllers/Page/KasublabController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Page;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class KasublabController extends Controller
+{
+    
+    public function pengaturan()
+    {
+
+    }
+
+}
