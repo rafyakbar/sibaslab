51fa488112823719a22d5ec7e757e7d1a6e799e8
Rafy Aulia Akbar <rafy683@gmail.com>
2018-01-26 10:22:46 +0700

    KonfirmasiSeeder

--- database/seeds/KonfirmasiSeeder.php
+++ database/seeds/KonfirmasiSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Fakultas;
+use App\Support\Role;
+use Faker\Factory;
 
 class KonfirmasiSeeder extends Seeder
 {
@@ -11,6 +14,34 @@ class KonfirmasiSeeder extends Seeder
      */
     public function run()
     {
-
+        $faker = Factory::create();
+        foreach (Fakultas::all() as $fakultas){
+            foreach ($fakultas->getJurusan() as $jurusan){
+                foreach ($jurusan->getMahasiswa() as $mahasiswa){
+                    $konfirmasi = rand(0,1);
+                    $konfirmasi = ($konfirmasi) ? $konfirmasi : rand(0,1);
+                    $dosen = $jurusan->getRelasiUser();
+                    $kasublabs = $dosen->whereNotIn('role', [Role::KALAB])->get();
+                    if ($konfirmasi){
+                        foreach ($dosen->get() as $d){
+                            $d->getRelasiMahasiswa()->attach($mahasiswa, [
+                                'disetujui' => true
+                            ]);
+                        }
+                        $mahasiswa->konfirmasi = true;
+                        $mahasiswa->save();
+                    }
+                    else{
+                        foreach ($kasublabs as $kasublab){
+                            $disetujui = rand(0,1);
+                            $kasublab->getRelasiMahasiswa()->attach($mahasiswa, [
+                                'disetujui' => $disetujui,
+                                'catatan' => ($disetujui) ? null : $faker->text
+                            ]);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
