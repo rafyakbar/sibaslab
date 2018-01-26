5f31c6bc58c543edf28311a9dcbc79dccf6d2db7
Rafy Aulia Akbar <rafy683@gmail.com>
2018-01-26 10:17:53 +0700

    penambahan migrasi fakultas

---
+++ database/migrations/2013_01_20_050050_fakultas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FakultasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fakultas', function (Blueprint $table){
+            $table->increments('id');
+            $table->string('nama');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
