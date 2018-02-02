8a749614b5c4941493ab077e0e351d4bf4d92663
BagasMuharom <bagashidayat45@gmail.com>
2018-02-02 15:00:36 +0700

    Menambah method loadMoreMahasiswa

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -13,7 +13,11 @@ class MahasiswaController extends Controller
     public function __construct()
     {
         $this->middleware('ajax')->only([
-            'setujuiSurat', 'tolakSurat'
+            'setujuiSurat', 'tolakSurat', 'loadMoreMahasiswa'
+        ]);
+
+        $this->middleware('role:KALAB')->only([
+            'loadMoreMahasiswa'
         ]);
     }
 
@@ -25,6 +29,20 @@ class MahasiswaController extends Controller
     public function perbaruiPassword()
     {
 
+    }
+
+    public function loadMoreMahasiswa(Request $request)
+    {  
+        $status = (int) $request->status;
+        $countLoaded = (int) $request->count_loaded;
+        
+        $daftarMahasiswa = Mahasiswa::getMahasiswaByStatus(Auth::user(), $status)->slice($countLoaded, 10);
+        
+        $daftarMahasiswa = $daftarMahasiswa->map(function ($item) {
+            return $item->setHidden(['password']);
+        });
+
+        return response()->json($daftarMahasiswa->toArray());
     }
 
     public function olahData(Request $request)
