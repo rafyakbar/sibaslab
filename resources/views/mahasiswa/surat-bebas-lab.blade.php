733984369a3e032852962016ca84c6a707c6ea8a
Unknown <iskandarjava@yahoo.co.id>
2018-02-04 20:35:49 +0700

    Penambahan layout surat

---
+++ resources/views/mahasiswa/surat-bebas-lab.blade.php
@@ -0,0 +1,54 @@
+<table width="100%"  border="0" cellspacing="5" cellpadding="0">
+    <tr valign="top">
+        <td width="10%"><img width="80" src="https://siakadu.unesa.ac.id/assets/images/logobaru.jpg"></td>
+        <td width="52%"><div style="font-size:13pt;margin-bottom:5px" id="head-title">KEMENTERIAN RISET, TEKNOLOGI, DAN PENDIDIKAN TINGGI</div>
+            <div id="head-big">UNIVERSITAS NEGERI SURABAYA</div></td>
+        <td width="38%"><table class="identitas" cellspacing="0" cellpadding="0">
+                <tbody>
+                <tr>
+                    <td colspan="3">Kampus Ketintang, Surabaya - 60213</td>
+                </tr>
+                <tr>
+                    <td>Telepon</td>
+                    <td>:</td>
+                    <td>+6231-99424932</td>
+                </tr>
+                <tr>
+                    <td>Faksimile</td>
+                    <td>:</td>
+                    <td>+6231-99424932</td>
+                </tr>
+                <tr>
+                    <td>e-mail</td>
+                    <td>:</td>
+                    <td>bakpk@unesa.ac.id</td>
+                </tr>
+                </tbody>
+            </table></td>
+    </tr>
+</table>
+<hr align="center" width="771">
+<br>
+<table width=650>
+    <tr>
+        <td align=center><font size=3><b><u>DAFTAR BEBAS LABORATORIUM</u></b></font></td>
+    </tr>
+    <tr>
+        <td align=center><b>JURUSAN {{ $jurusan->nama }} - FT.UNESA</b></td>
+    </tr>
+</table>
+<br>
+<table width="">
+    <tr>
+        <td nowrap><font size="-1"><strong>NIM</strong></font></td>
+        <td width="150" nowrap><font size="-1">: 15051204038 </font></td>
+    </tr>
+    <tr>
+        <td nowrap><font size="-1"><strong>Nama</strong></font></td>
+        <td nowrap><font size="-1">: MUHAMMAD ISKANDAR JAVA </font></td>
+    </tr>
+    <tr>
+        <td nowrap><font size="-1"><strong>Program Studi</strong></font></td>
+        <td><font size="-1">: {{ $prodi->nama }} </font></td>
+    </tr>
+</table>
