5119507a602f920e50d15f21c47d81e8676b0990
BagasMuharom <bagashidayat45@gmail.com>
2018-02-06 17:49:31 +0700

    Memindah route mahasiswa

--- routes/mahasiswa.php
+++ routes/mahasiswa.php
@@ -1,3 +1,38 @@
 <?php
 
-// route mahasiswa
+Route::group(['prefix' => 'mahasiswa'], function () {
+
+    Route::get('dashboard', [
+        'uses'=>'MahasiswaController@dashboard',
+        'as'=>'mahasiswa.dashboard'
+    ]);
+
+    Route::get('ajukan',[
+        'uses' => 'MahasiswaController@ajukan',
+        'as' => 'mahasiswa.ajukan'
+    ]);
+    Route::get('edit', [
+        'uses' => 'MahasiswaController@edit',
+        'as' => 'mahasiswa.edit'
+    ]);
+    Route::post('ajukan', [
+        'uses' => 'MahasiswaController@prosesAjukan',
+        'as' => 'mahasiswa.ajukan.proses'
+    ]);
+    Route::post('perbaruiBerkas', [
+        'uses' => 'MahasiswaController@perbaruiBerkas',
+        'as' => 'mahasiswa.berkas.perbarui'
+    ]);
+    Route::post('perbaruiPassword', [
+        'uses' => 'MahasiswaController@perbaruiPassword',
+        'as' => 'mahasiswa.password.perbarui'
+    ]);
+    Route::get('etc/getprodi/{jurusan_id}', [
+        'uses' => 'PublicController@getProdi',
+        'as' => 'mahasiswa.etc.getjurusan'
+    ]);
+    Route::get('unduh', [
+        'uses' => 'MahasiswaController@unduh',
+        'as' => 'mahasiswa.unduh'
+    ]);
+});
--- routes/web.php
+++ routes/web.php
@@ -26,11 +26,6 @@ Route::namespace('Auth')->group(function () {
 
 Route::group(['prefix' => 'mahasiswa'], function () {
 
-    Route::get('dashboard', [
-        'uses'=>'MahasiswaController@dashboard',
-        'as'=>'mahasiswa.dashboard'
-    ]);
-
     Route::get('login', [
         'uses' => 'MahasiswaController@login',
         'as' => 'mahasiswa.login'
@@ -41,34 +36,6 @@ Route::group(['prefix' => 'mahasiswa'], function () {
         'as' => 'mahasiswa.login.proses'
     ]);
 
-    Route::get('ajukan',[
-        'uses' => 'MahasiswaController@ajukan',
-        'as' => 'mahasiswa.ajukan'
-    ]);
-    Route::get('edit', [
-        'uses' => 'MahasiswaController@edit',
-        'as' => 'mahasiswa.edit'
-    ]);
-    Route::post('ajukan', [
-        'uses' => 'MahasiswaController@prosesAjukan',
-        'as' => 'mahasiswa.ajukan.proses'
-    ]);
-    Route::post('perbaruiBerkas', [
-        'uses' => 'MahasiswaController@perbaruiBerkas',
-        'as' => 'mahasiswa.berkas.perbarui'
-    ]);
-    Route::post('perbaruiPassword', [
-        'uses' => 'MahasiswaController@perbaruiPassword',
-        'as' => 'mahasiswa.password.perbarui'
-    ]);
-    Route::get('etc/getprodi/{jurusan_id}', [
-        'uses' => 'PublicController@getProdi',
-        'as' => 'mahasiswa.etc.getjurusan'
-    ]);
-    Route::get('unduh', [
-        'uses' => 'MahasiswaController@unduh',
-        'as' => 'mahasiswa.unduh'
-    ]);
 });
 
 Route::namespace('Page')->group(function () {
