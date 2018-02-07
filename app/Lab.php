655a1e5b7b038e93e13bc40e587c3a2a6f162b41
Rafy Aulia Akbar <rafy683@gmail.com>
2018-02-07 17:25:02 +0700

    update model Lab

--- app/Lab.php
+++ app/Lab.php
@@ -13,4 +13,22 @@ class Lab extends Model
     protected $fillable = [
         'nama', 'kelas'
     ];
+
+    /**
+     * mendapatkan relasi user
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function getRelasiUser()
+    {
+        return $this->hasMany('App\User', 'lab_id');
+    }
+
+    /**
+     * mendapatkan data user
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function getUser()
+    {
+        return $this->getRelasiUser()->get();
+    }
 }
