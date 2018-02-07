14d04345431057f96f10032ee18a954dc1c91b21
Rafy Aulia Akbar <rafy683@gmail.com>
2018-02-07 17:28:40 +0700

    urutan seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call('MahasiswaSeeder');
         $this->call('KonfirmasiSeeder');
         $this->call('RootSeeder');
+        $this->call('LabSeeder');
     }
 }
