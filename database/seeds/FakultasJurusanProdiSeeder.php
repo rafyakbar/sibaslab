1f28e8aa3abbdc5c796b684629145c3fe0fa21bd
Rafy Aulia Akbar <rafy683@gmail.com>
2018-02-10 17:41:02 +0700

    seeder (hanya untuk ft)

--- database/seeds/FakultasJurusanProdiSeeder.php
+++ database/seeds/FakultasJurusanProdiSeeder.php
@@ -41,144 +41,144 @@ class FakultasJurusanProdiSeeder extends Seeder
             ]
         ],
 
-        'Ilmu Pendidikan' => [
-            'Bimbingan & Konseling' => [
-                'S1 Bimbingan Konseling'
-            ],
-            'Pendidikan Luar Biasa' => [
-                'S1 Pendidikan Luar Biasa'
-            ],
-            'Pendidikan Luar Sekolah' => [
-                'S1 Pendidikan Luar Sekolah'
-            ],
-            'Manajemen Pendidikan' => [
-                'S1 Manajemen Pendidikan'
-            ],
-            'PG -PAUD' => [
-                'S1 PG - PAUD'
-            ],
-            'Psikologi' => [
-                'S1 Psikologi'
-            ],
-            'Teknologi Pendidikan' => [
-                'S1 Teknologi Pendidikan'
-            ],
-            'Pendidikan Guru Sekolah Dasar' => [
-                'S1 Pendidikan Guru Sekolah Dasar'
-            ]
-        ],
-
-        'Bahasa & Seni' => [
-            'Bahasa & Sastra Indonesia' => [
-                'S1 Pendidikan Bahasa Indonesia',
-                'S1 Sastra Indonesia'
-            ],
-            'Bahasa & Sastra Inggris' => [
-                'S1 Pendidikan Bahasa Inggris',
-                'S1 Sastra Inggris',
-                'D3 Bahasa Inggris'
-            ],
-            'Bahasa & Sastra Daerah' => [
-                'S1 Pendidikan Bahasa Jawa',
-                'S1 Sastra Jawa'
-            ],
-            'Bahasa & Sastra Jerman' => [
-                'S1 Pendidikan Bahasa Jerman',
-                'S1 Sastra Jerman'
-            ],
-            'Bahasa & Sastra Jepang' => [
-                'S1 Pendidikan Bahasa Jepang',
-            ],
-            'Bahasa & Sastra Mandarin' => [
-                'S1 Pendidikan Bahasa Mandarin'
-            ],
-            'Seni Rupa' => [
-                'S1 Pendidikan Seni Rupa',
-                'S1 Seni Rupa'
-            ],
-            'Desain' => [
-                'S1 Desain Komunikasi Visual',
-                'D3 Desain Grafis'
-            ],
-            'Seni Drama, Tari & Musik' => [
-                'S1 Pendidikan Seni Drama, Tari, & Musik',
-                'S1 Seni Musik',
-            ],
-        ],
-
-        'Matematika & Ilmu Pengetahuan Alam' => [
-            'Matematika' => [
-                'S1 Matematika',
-                'S1 Pendidikan Matematika'
-            ],
-            'Fisika' => [
-                'S1 Fisika',
-                'S1 Pendidikan Fisika'
-            ],
-            'Kimia' => [
-                'S1 Pendidikan Kimia',
-                'S1 Kimia'
-            ],
-            'Biologi' => [
-                'S1 Biologi',
-                'S1 Pendidikan Biologi'
-            ],
-            'Pendidikan IPA' => [
-                'S1 Pendidikan IPA',
-            ],
-        ],
-
-        'Ilmu Sosial & Hukum' => [
-            'PMP-Kn' => [
-                'S1 Pendidikan Pancasila & Kewarganegaraan'
-            ],
-            'Administrasi Publik' => [
-                'S1 Ilmu Administrasi Negara',
-                'D3 Administrasi Negara'
-            ],
-            'Hukum' => [
-                'S1 Ilmu Hukum'
-            ],
-            'Pendidikan Geografi' => [
-                'S1 Pendidikan Geografi',
-                'S1 Pendidikan IPS'
-            ],
-            'Pendidikan Sejarah' => [
-                'S1 Pendidikan Sejarah',
-            ],
-            'Ilmu Sosial' => [
-                'S1 Sosiologi',
-                'S1 Ilmu komunikasi'
-            ],
-        ],
-
-        'Ilmu Keolahragaan' => [
-            'Ilmu Keolahragaan' => [
-                'S1 Pendidikan Kepelatihan Olahraga',
-                'S1 Ilmu Keolahragaan',
-                'S1 Pend. Jasmani, Kesehatan, & Rekreasi'
-            ]
-        ],
-
-        'Ekonomi' => [
-            'Pendidikan Ekonomi' => [
-                'S1 Pendidikan Ekonomi',
-                'S1 Pendidikan Administrasi Perkantoran',
-                'S1 Pendidikan Akuntansi',
-                'S1 Pendidikan Tata Niaga',
-            ],
-            'Manajemen' => [
-                'S1 Manajemen',
-            ],
-            'Akutansi' => [
-                'S1 Akuntansi',
-                'D3 Akuntansi'
-            ],
-            'Ilmu Ekonomi' => [
-                'S1 Ekonomi Islam',
-                'S1 Ilmu Ekonomi'
-            ]
-        ]
+//        'Ilmu Pendidikan' => [
+//            'Bimbingan & Konseling' => [
+//                'S1 Bimbingan Konseling'
+//            ],
+//            'Pendidikan Luar Biasa' => [
+//                'S1 Pendidikan Luar Biasa'
+//            ],
+//            'Pendidikan Luar Sekolah' => [
+//                'S1 Pendidikan Luar Sekolah'
+//            ],
+//            'Manajemen Pendidikan' => [
+//                'S1 Manajemen Pendidikan'
+//            ],
+//            'PG -PAUD' => [
+//                'S1 PG - PAUD'
+//            ],
+//            'Psikologi' => [
+//                'S1 Psikologi'
+//            ],
+//            'Teknologi Pendidikan' => [
+//                'S1 Teknologi Pendidikan'
+//            ],
+//            'Pendidikan Guru Sekolah Dasar' => [
+//                'S1 Pendidikan Guru Sekolah Dasar'
+//            ]
+//        ],
+//
+//        'Bahasa & Seni' => [
+//            'Bahasa & Sastra Indonesia' => [
+//                'S1 Pendidikan Bahasa Indonesia',
+//                'S1 Sastra Indonesia'
+//            ],
+//            'Bahasa & Sastra Inggris' => [
+//                'S1 Pendidikan Bahasa Inggris',
+//                'S1 Sastra Inggris',
+//                'D3 Bahasa Inggris'
+//            ],
+//            'Bahasa & Sastra Daerah' => [
+//                'S1 Pendidikan Bahasa Jawa',
+//                'S1 Sastra Jawa'
+//            ],
+//            'Bahasa & Sastra Jerman' => [
+//                'S1 Pendidikan Bahasa Jerman',
+//                'S1 Sastra Jerman'
+//            ],
+//            'Bahasa & Sastra Jepang' => [
+//                'S1 Pendidikan Bahasa Jepang',
+//            ],
+//            'Bahasa & Sastra Mandarin' => [
+//                'S1 Pendidikan Bahasa Mandarin'
+//            ],
+//            'Seni Rupa' => [
+//                'S1 Pendidikan Seni Rupa',
+//                'S1 Seni Rupa'
+//            ],
+//            'Desain' => [
+//                'S1 Desain Komunikasi Visual',
+//                'D3 Desain Grafis'
+//            ],
+//            'Seni Drama, Tari & Musik' => [
+//                'S1 Pendidikan Seni Drama, Tari, & Musik',
+//                'S1 Seni Musik',
+//            ],
+//        ],
+//
+//        'Matematika & Ilmu Pengetahuan Alam' => [
+//            'Matematika' => [
+//                'S1 Matematika',
+//                'S1 Pendidikan Matematika'
+//            ],
+//            'Fisika' => [
+//                'S1 Fisika',
+//                'S1 Pendidikan Fisika'
+//            ],
+//            'Kimia' => [
+//                'S1 Pendidikan Kimia',
+//                'S1 Kimia'
+//            ],
+//            'Biologi' => [
+//                'S1 Biologi',
+//                'S1 Pendidikan Biologi'
+//            ],
+//            'Pendidikan IPA' => [
+//                'S1 Pendidikan IPA',
+//            ],
+//        ],
+//
+//        'Ilmu Sosial & Hukum' => [
+//            'PMP-Kn' => [
+//                'S1 Pendidikan Pancasila & Kewarganegaraan'
+//            ],
+//            'Administrasi Publik' => [
+//                'S1 Ilmu Administrasi Negara',
+//                'D3 Administrasi Negara'
+//            ],
+//            'Hukum' => [
+//                'S1 Ilmu Hukum'
+//            ],
+//            'Pendidikan Geografi' => [
+//                'S1 Pendidikan Geografi',
+//                'S1 Pendidikan IPS'
+//            ],
+//            'Pendidikan Sejarah' => [
+//                'S1 Pendidikan Sejarah',
+//            ],
+//            'Ilmu Sosial' => [
+//                'S1 Sosiologi',
+//                'S1 Ilmu komunikasi'
+//            ],
+//        ],
+//
+//        'Ilmu Keolahragaan' => [
+//            'Ilmu Keolahragaan' => [
+//                'S1 Pendidikan Kepelatihan Olahraga',
+//                'S1 Ilmu Keolahragaan',
+//                'S1 Pend. Jasmani, Kesehatan, & Rekreasi'
+//            ]
+//        ],
+//
+//        'Ekonomi' => [
+//            'Pendidikan Ekonomi' => [
+//                'S1 Pendidikan Ekonomi',
+//                'S1 Pendidikan Administrasi Perkantoran',
+//                'S1 Pendidikan Akuntansi',
+//                'S1 Pendidikan Tata Niaga',
+//            ],
+//            'Manajemen' => [
+//                'S1 Manajemen',
+//            ],
+//            'Akutansi' => [
+//                'S1 Akuntansi',
+//                'D3 Akuntansi'
+//            ],
+//            'Ilmu Ekonomi' => [
+//                'S1 Ekonomi Islam',
+//                'S1 Ilmu Ekonomi'
+//            ]
+//        ]
     ];
 
     /**
