17ec76b602e93b10440193066411c1feb6e61f40
Unknown <iskandarjava@yahoo.co.id>
2018-02-10 20:46:12 +0700

    Update kalab controller

--- app/Http/Controllers/Page/KalabController.php
+++ app/Http/Controllers/Page/KalabController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Page;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class KalabController extends Controller
 {
