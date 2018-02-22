607fa250db3d38676476bd9a9c2b521bc62c208a
Rafy Aulia Akbar <rafy683@gmail.com>
2018-02-22 19:18:18 +0700

    update lgi cooy

--- database/migrations/2018_01_20_063552_mahasiswa_table.php
+++ database/migrations/2018_01_20_063552_mahasiswa_table.php
@@ -24,7 +24,7 @@ class MahasiswaTable extends Migration
                 ->onUpdate('CASCADE')
                 ->onDelete('SET NULL');
             $table->string('nama');
-            $table->text('email')->nullable();
+            $table->text('email')->default('');
             $table->float('ipk')->default(0);
             $table->string('ta')->nullable();
             $table->boolean('konfirmasi')->default(false);
