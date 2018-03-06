a8cfbd63678d9b3c64864df243f5b6c118778604
BagasMuharom <bagashidayat45@gmail.com>
2018-03-06 13:25:33 +0700

    Fixing typo

--- app/Listeners/KirimEmailBelumDisetujui.php
+++ app/Listeners/KirimEmailBelumDisetujui.php
@@ -5,7 +5,7 @@ namespace App\Listeners;
 use App\Events\SuratBelumDisetujui;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Support\Fadaces\Mail;
+use Illuminate\Support\Facades\Mail;
 use App\Mail\SuratBelumDisetujuiMail;
 
 class KirimEmailBelumDisetujui
