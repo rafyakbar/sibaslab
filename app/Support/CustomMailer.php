177b031d6c0c67670f1a3f43f2cf301361edfcf4
BagasMuharom <bagashidayat45@gmail.com>
2018-03-08 20:39:22 +0700

    Fixing masalah email

---
+++ app/Support/CustomMailer.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Support;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception as PHPMailerException;
+
+class CustomMailer {
+
+    protected $subject;
+
+    protected $to = [];
+
+    public function __construct()
+    {
+        $this->subject = 'Surat Bebas Lab';
+    }
+
+    public function to($users)
+    {
+        array_push($this->to, $users);
+
+        return $this;
+    }
+
+    public function subject($subject) {
+        $this->subject = $subject;
+        return $this;
+    }
+
+    public function send($view, $data = [])
+    {
+            $mail = new PHPMailer(true);
+
+            try {
+                $mail->isSMTP();
+                $mail->SMTPDebug = true;
+                $mail->Host = $_ENV['MAIL_HOST'];
+                $mail->SMTPAuth = true;
+                $mail->Username = $_ENV['MAIL_USERNAME'];
+                $mail->Password = $_ENV['MAIL_PASSWORD'];
+                $mail->SMTPSecure = $_ENV['MAIL_ENCRYPTION'];
+                $mail->Port = $_ENV['MAIL_PORT'];
+                $mail->setFrom($_ENV['MAIL_USERNAME'], 'Surat Bebas Lab');
+
+                $mail->SMTPOptions= array(
+                    'ssl' => array(
+                        'verify_peer' => false,
+                        'verify_peer_name' => false,
+                        'allow_self_signed' => true
+                    )
+                );
+
+                foreach($this->to as $to) {
+                    $mail->addAddress($to);
+                }
+
+                $mail->isHTML(true);
+                $mail->Subject = $this->subject;
+
+                if(is_string($view)) {
+                    $mail->Body = view($view, $data)->render();
+                }
+                else {
+                    $mail->Body = $view->build()->render();
+                }
+                $mail->send();
+            }
+            catch (PHPMailerException $err) {
+                echo 'Gagal mengirim email';
+            } catch (\Throwable $e) {
+                echo 'Gagal !';
+            }
+    }
+
+}
